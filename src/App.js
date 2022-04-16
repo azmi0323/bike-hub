908e15e3ada2273d3a1d268d98c71bf4375fcd14
AzmiHabib <azmihabib0323@gmail.com>
2022-04-16 21:49:17 +0600

    header design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App text-info">
-     <h1>Hello</h1>
+    <div >
+      <Header></Header>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './Header.css'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div>
-            <h1>This is header</h1>
-        </div>
-    );
+  return (
+    <div className="header">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand >Capture<span className="text-info fw-bold">The</span>Moment</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Link to="/">Home</Link>
+            <Link to="/orders">Blogs</Link>
+            <Link to="/reviews">About</Link>
+            <Link to="/register">Contact</Link>
+            </Nav>
+            <Nav>
+              <button>Log In</button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
