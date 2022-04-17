f8bf4076990f8a5e4dcd62c41579e399d778d709
AzmiHabib <azmihabib0323@gmail.com>
2022-04-17 13:57:54 +0600

    commit after fetching all product

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousels from "../Carousels/Carousels";
 import "./Home.css";
 const Home = () => {
+
+  const [products,setProducts]=useState([]);
+  useEffect(()=>{
+    fetch('product.json')
+    .then(res=>res.json())
+    .then(data=>setProducts(data))
+  },[])
+console.log(products);
   return (
     <div>
       <Carousels></Carousels>
